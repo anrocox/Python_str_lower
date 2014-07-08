3a7b0a8826e199c07fbdb8730702e54250da74a6
Andres Rodriguez <anrocox@gmail.com>
2014-07-08 12:44:28 -0500

    How to convert a string to lowercase in a python str?

---
+++ src/lower_str.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 8, 2014
+
+@author: anroco
+
+How to convert a string to lowercase in a python str?
+
+¿Como convertir los caracteres de un str python en minuscalas?
+'''
+
+#create a str
+s = 'Example String in Python'
+print(s)
+
+#the lower() method generates copy of the string with all the characters
+#converted to lowercase.
+s_lower = s.lower()
+print(s_lower)
+print(s)
+
+#create a str
+s = 'CREATE 2 GAMES IN 1 MINUTE'
+print(s)
+
+#return a new string.
+s_lower = s.lower()
+print(s_lower)
